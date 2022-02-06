924bfeab2c944fdfade9c6b0167bca0117ffb63d
RED-NTWRK <61582264+RED-NTWRK@users.noreply.github.com>
2022-02-06 11:02:06 +0300

    new website design

---
+++ scripts/form-1a69.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => 'mohammed.tamimi@reposecafe.sa',
+    'to' => 'mohammed.tamimi@reposecafe.sa'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'phone' => array(
+    'order' => 2,
+    'type' => 'tel',
+    'label' => 'Phone',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Phone\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 3,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'select' => array(
+    'order' => 4,
+    'type' => 'string',
+    'label' => 'select',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'select\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 5,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
